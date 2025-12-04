00eb5b55c1e4743a53142174c7b01920657839e6
jasmatp <parmarjasmat@gmail.com>
2025-12-04 10:21:13 +0530

    update port

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://aj-talk.vercel.app/",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
